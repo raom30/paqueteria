faf19777c384547304c595c6204db165c274bfde
Rafa <raom30@hotmail.com>
2018-04-04 11:54:21 +0200

    Commit

---
+++ src/main/java/es/paqueteria/paqueteria/PaqueteriaApplication.java
@@ -0,0 +1,12 @@
+package es.paqueteria.paqueteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaqueteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaqueteriaApplication.class, args);
+	}
+}
