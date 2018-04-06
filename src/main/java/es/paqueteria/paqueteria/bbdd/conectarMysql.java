149ad857c80dc50db11358b3d2f77b8b043d6e57
Rafa <raom30@hotmail.com>
2018-04-06 14:18:23 +0200

    Commit 06/04/2018

---
+++ src/main/java/es/paqueteria/paqueteria/bbdd/conectarMysql.java
@@ -0,0 +1,48 @@
+package es.paqueteria.paqueteria.bbdd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class conectarMysql {
+
+	private static Connection Conexion;
+
+	public void MySQLConnection(String user, String pass, String db_name) throws Exception {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + db_name, user, pass);
+			JOptionPane.showMessageDialog(null, "Se ha iniciado la conexión con el servidor de forma exitosa");
+		} catch (ClassNotFoundException ex) {
+			Logger.getLogger(conectarMysql.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (SQLException ex) {
+			Logger.getLogger(conectarMysql.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public void closeConnection() {
+		try {
+			Conexion.close();
+			System.out.println("Se ha finalizado la conexión con el servidor");
+		} catch (SQLException ex) {
+			Logger.getLogger(conectarMysql.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		conectarMysql db = new conectarMysql();
+
+		try {
+			db.MySQLConnection("root", "", "test");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
